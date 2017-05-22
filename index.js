eccc49d93990b799bb5b5e360a827091558b7b14
gabystern <stern.gabrielle@gmail.com>
2017-05-22 15:49:54 -0400

    image keyword and artist search

--- index.js
+++ index.js
@@ -27,30 +27,30 @@ $(document).ready(function(){
     })
 })
 
-// $('form#image-author-search').on('submit', function(event){
-//   event.preventDefault()
-//   console.log("Submitted Form!!!")
-//   // make the search term dynamic at the end of the url
-//   const author_query = $('#author_query').val()
-//   var url = `https://api.gettyimages.com/v3/search/images?phrase=${author_query}`
-//   $('#author_query').val('')
-//
-//   $.ajax({
-//     url: url,
-//     success: function(data){
-//       console.log(data)
-//       const lis = data.images.map(function(image){
-//         if (image.caption !== null) {
-//           // const caption =  `${image.caption}`
-//           return `<li> <img src = "${image.display_sizes[0].uri}"> <br> ${image.caption} <br><br> </li>`
-//         }
-//       })
-//       $('#images').html(lis.join(''))
-//     },
-//     headers: {
-//       'Api-Key': 'q8tqktcyna5whgb5ktuf49je'
-//     }
-//   })
-// })
+$('form#image-author-search').on('submit', function(event){
+  event.preventDefault()
+  console.log("Submitted Form!!!")
+  // make the search term dynamic at the end of the url
+  const author_query = $('#author_query').val()
+  var url = `https://api.gettyimages.com/v3/search/images?artists=${author_query}`
+  $('#author_query').val('')
+
+  $.ajax({
+    url: url,
+    success: function(data){
+      console.log(data)
+      const lis = data.images.map(function(image){
+        if (image.caption !== null) {
+          // const caption =  `${image.caption}`
+          return `<li> <img src = "${image.display_sizes[0].uri}"> <br> ${image.caption} <br><br> </li>`
+        }
+      })
+      $('#images').html(lis.join(''))
+    },
+    headers: {
+      'Api-Key': 'q8tqktcyna5whgb5ktuf49je'
+    }
+  })
+})
 
 })

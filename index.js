df72fa03a97a34b00cdd3e1e23cbf13249504d0a
gabystern <stern.gabrielle@gmail.com>
2017-06-07 12:59:27 -0400

    cleanup

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
   console.log('Document ready!')
+  keywordSearch()
+  artistSearch()
+})
 
+function keywordSearch(){
   $('form#image-search').on('submit', function(event){
     event.preventDefault()
     console.log("Submitted Form!!!")
-    // make the search term dynamic at the end of the url
     const query = $('#query').val()
     var url = `https://api.gettyimages.com/v3/search/images?phrase=${query}`
     $('#query').val('')
@@ -12,10 +15,8 @@ $(document).ready(function(){
     $.ajax({
       url: url,
       success: function(data){
-        console.log(data)
         const lis = data.images.map(function(image){
           if (image.caption !== null) {
-            // const caption =  `${image.caption}`
             return `<li> <img src = "${image.display_sizes[0].uri}"> <br>${image.caption}<br> <br></li>`
           }
         })
@@ -25,31 +26,30 @@ $(document).ready(function(){
         'Api-Key': 'q8tqktcyna5whgb5ktuf49je'
       }
     })
-})
-
-$('form#image-author-search').on('submit', function(event){
-  event.preventDefault()
-  console.log("Submitted Form!!!")
-  // make the search term dynamic at the end of the url
-  const author_query = $('#author_query').val()
-  var url = `https://api.gettyimages.com/v3/search/images?artists=${author_query}`
-  $('#author_query').val('')
-
-  $.ajax({
-    url: url,
-    success: function(data){
-      console.log(data)
-      const lis = data.images.map(function(image){
-        if (image.caption !== null) {
-          return `<li> <img src = "${image.display_sizes[0].uri}"><br>${image.caption}<br> <br> </li>`
-        }
-      })
-      $('#images').html(lis.join(''))
-    },
-    headers: {
-      'Api-Key': '63dnpue5dw2pb5ayaqw9gn2d'
-    }
   })
-})
+}
 
-})
+function ArtistSearch(){
+  $('form#image-author-search').on('submit', function(event){
+    event.preventDefault()
+    console.log("Submitted Form!!!")
+    const author_query = $('#author_query').val()
+    var url = `https://api.gettyimages.com/v3/search/images?artists=${author_query}`
+    $('#author_query').val('')
+
+    $.ajax({
+      url: url,
+      success: function(data){
+        const lis = data.images.map(function(image){
+          if (image.caption !== null) {
+            return `<li> <img src = "${image.display_sizes[0].uri}"><br>${image.caption}<br> <br> </li>`
+          }
+        })
+        $('#images').html(lis.join(''))
+      },
+      headers: {
+        'Api-Key': '63dnpue5dw2pb5ayaqw9gn2d'
+      }
+    })
+  })
+}
